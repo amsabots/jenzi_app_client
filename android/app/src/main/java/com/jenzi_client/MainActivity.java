f19799b864c6c557087125a92558587295daa98b
Andrew Mititi <andymwebi@gmail.com>
2022-06-30 20:56:26 +0300

    Fix: hooking the system to the new API system provider

--- android/app/src/main/java/com/jenzi_client/MainActivity.java
+++ android/app/src/main/java/com/jenzi_client/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Jenzi Client";
+    return "Jenzi Smart";
   }
 
   @Override
